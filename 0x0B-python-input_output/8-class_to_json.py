73d788aed3cb59bdc314cb4e0b0f4590abfbad4b
wangarisk <w.skinuthia@gmail.com>
2023-04-12 23:16:36 +0000

    task8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""defines a Python class-to-JSON function."""
-
 
 def class_to_json(obj):
-    """Return the dictionary represntation of a simple data structure."""
     return obj.__dict__
-
