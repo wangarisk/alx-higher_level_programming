e6fbdf4321fb1008efbc0c121e70b56bbd38c4b6
wangarisk <w.skinuthia@gmail.com>
2023-04-14 19:55:52 +0000

    task1correct

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
+"""Def a class square."""
+
+
 class Square:
+    """Rep a square"""
 
     def __init__(self, size):
         """Initialize new square
-        Args
-            size - size new square
+        Args:
+            size - size of new square
         """
         self.__size = size
