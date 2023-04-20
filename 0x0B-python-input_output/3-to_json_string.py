f53c634778d6f8f24bfd221f84a1ed3413d4b21d
wangarisk <w.skinuthia@gmail.com>
2023-04-20 21:43:52 +0000

    delete 7 - B

--- 0x0B-python-input_output/3-to_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""defines a string-to-JSON fn"""
-import json
-
-
-def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
-    return json.dumps(my_obj)
--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-def class_to_json(obj):
-    """Returns the dictionary representation of a simple data structure"""
-    return obj.__dict__
